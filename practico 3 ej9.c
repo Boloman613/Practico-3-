878ff7ccdb8218a56b7fe8c71923e199f3fca04b
Boloman613 <villegasaxel9412@gmail.com>
2025-10-06 18:13:05 -0400

    Add files via upload

---
+++ practico 3 ej9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int n, i;
+    double x, suma = 0, fac;
+    printf("Ingrese X y n: ");
+    scanf("%lf %d", &x, &n);
+    for(i = 1; i <= n; i++) {
+        fac = 1;
+        for(int j = 1; j <= i; j++)
+            fac *= j;
+        if(i % 2 == 0)
+            suma -= pow(x, i) / fac;
+        else
+            suma += pow(x, i) / fac;
+    }
+    printf("Suma = %.4f\n", suma);
+    return 0;
+}
